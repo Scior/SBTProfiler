d17708cc8a2f533b9dd1f338841f2f1c78cb401e
Suita Fujino <interval12@gmail.com>
2019-10-01 20:22:33 +0900

    Fix for a bug

--- src/profiler.py
+++ src/profiler.py
@@ -29,10 +29,10 @@ def read_activity_log(path):
     with gzip.open(args.file, mode='rt') as file:
         prog = re.compile('^\d+\.\d+ms')
         profiles = set()
-        for item in file.readline():
+        for item in file.readlines():
             if prog.match(item):
                 items = item.split('\t', 1)
-                profiles.add((float(items[0].strip('ms')), items[1]))
+                profiles.add((float(items[0].strip('ms')), items[1].strip()))
 
     return list(profiles)
 
